4cb86fb91de9e42c1da2518ca02914a4d598366a
faineBenk <tinagreen@mail.ru>
2022-07-24 01:18:37 +0300

    working with multiple packages

--- init_script.sh
+++ init_script.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
-PACKAGE=$1
-export PACKAGE=$1
-echo  -----${PACKAGE} is checkouted now-----
+# PACKAGE=$1
+# export PACKAGE=$@
 
-pacman -Qi ${PACKAGE}
-export IS_ALREADY_EXISTS=$?
+export PROCESS_PCKGS="$@"
+declare -A PCKG_DICT 
 
-if [ $IS_ALREADY_EXISTS -eq 0 ]; then 
-	echo "-----${PACKAGE} is already installed. skipping-----"
-	export RES=0
-	# exit 0
-elif [ $IS_ALREADY_EXISTS -eq 1 ]; then
-	yes | pacman -Sy ${PACKAGE}
-	export RES=$? 
-	if [ $RES -eq 0 ]; then
-		echo "-----${PACKAGE} was succesfully installed-----"
+
+for i in $PROCESS_PCKGS; do
+	echo  -----$i is checkouted now-----
+
+	pacman -Qi $i
+	export IS_ALREADY_EXISTS=$?
+
+	if [ $IS_ALREADY_EXISTS -eq 0 ]; then 
+		echo "-----$i is already installed. skipping-----"
+		export RES=0
+		# PCKG_DICT[$i]=$IS_ALREADY_EXISTS
+		# IS_EXISTS=PCKG_DICT[$i]
+		# exit 0
+	elif [ $IS_ALREADY_EXISTS -eq 1 ]; then
+		yes | pacman -Sy $i
+		export RES=$? 
+		if [ $RES -eq 0 ]; then
+			echo "-----$i was succesfully installed-----"
+		else
+			echo "target not found in repositories"
+		fi
 	else
-		echo "target not found in repositories"
+		echo "-----error. Can't install.-----"
+		# PCKG_DICT[$i]=$IS_ALREADY_EXISTS
 	fi
-else
-	echo "-----error. Can't install.-----"
-fi
+done
+
 
 python3 window.py
+
--- window.py
+++ window.py
@@ -6,19 +6,44 @@ app = QApplication(sys.argv)
 
 already_exists = os.environ['IS_ALREADY_EXISTS']
 result = os.environ['RES']
-package = os.environ['PACKAGE']
+process_pckg = os.environ['PROCESS_PCKGS']
 
+# test_var_exists = os.environ['IS_EXISTS']
+# pckg_dict = os.environ['PCKG_DICT']
 
-def create_window(result, already_exists, package):
-	if (already_exists=='0'):
-		print("package already exists")
-		window = QLabel(package + "\nis already exists in system.")
-	elif (already_exists=='1' and result!="1"):
-		window = QLabel(package + "\nwas succesfully installed!")
-	else:
-		window = QLabel(package + "\nwas not installed.\nTry again.")
+# process_pckg = [
+# {name: 'pckg_name', 
+# res: 'res', 
+# already_exists: '1'},
+# {name: 'pckg_name', 
+# res: 'res', 
+# already_exists: '1'}
+# ]
+
+def create_window(result, already_exists, process_pckg):
+	# process_pckg = process_pckg.split()
+	print(type(process_pckg))
+	for i in process_pckg:
+		s = ""
+		if (already_exists=='0'):
+			print("package already exists")
+			window = QLabel(process_pckg + "\nis already exists in system.")
+			# s+= name + " is already exists in system.\n"
+
+		elif (already_exists=='1'):
+			print("package was successfully installed!")
+			window = QLabel(process_pckg + "\nwas succesfully installed!")
+			# s+= name + " was succesfully installed!\n"
+		else:
+			print("Oops.")
+			window = QLabel(process_pckg + "\nwas not installed.\nTry again.")
+			# s+= name + " was not installed. Try again.\n"
+
+	# window = QLabel(s)
 	window.setGeometry(50,50,150,50)
 	window.show()
 	app.exec()
 
-create_window(result, already_exists, package)
+
+
+create_window(result, already_exists, process_pckg)
